e55f7c789e60d7f12e687a5346001a5c96d9d5cd
Fahim Abrar <fahimabrar@appscode.com>
2019-04-19 12:17:05 +0600

    Add Test for a Pre-Provisioned Linode Block Storage PersistentVolumeClaim

--- e2e/test/csi_driver_test.go
+++ e2e/test/csi_driver_test.go
@@ -2,20 +2,24 @@ package test
 
 import (
 	"e2e_test/framework"
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	core "k8s.io/api/core/v1"
-	"time"
 )
 
 var _ = Describe("CSIDriver", func() {
 	var (
-		err error
-		pod *core.Pod
-		pvc *core.PersistentVolumeClaim
-		f   *framework.Invocation
-		size string
-		file = "/data/heredoc"
+		err      error
+		pod      *core.Pod
+		podName1 = "test-pod-1"
+		podName2 = "test-pod-2"
+		pvc      *core.PersistentVolumeClaim
+		f        *framework.Invocation
+		size     string
+		file     = "/data/heredoc"
+		waitTime = 1 * time.Minute
 	)
 	BeforeEach(func() {
 		f = root.Invoke()
@@ -33,6 +37,24 @@ var _ = Describe("CSIDriver", func() {
 		Expect(err).NotTo(HaveOccurred())
 	}
 
+	var waitForOperation = func() {
+		time.Sleep(waitTime)
+	}
+
+	var deleteAndCreatePod = func(name string) {
+		By("Deleting the First pod")
+		err = f.DeletePod(pod.Name)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("Waiting for the Volume to be Detached")
+		waitForOperation()
+
+		By("Creating Second Pod with the Same PVC")
+		pod = f.GetPodObject(name, pvc.Name)
+		err = f.CreatePod(pod)
+		Expect(err).NotTo(HaveOccurred())
+	}
+
 	Describe("Test", func() {
 		Context("Simple", func() {
 			Context("Block Storage", func() {
@@ -43,25 +65,25 @@ var _ = Describe("CSIDriver", func() {
 					Expect(err).NotTo(HaveOccurred())
 
 					By("Creating Pod with PVC")
-					pod = f.GetPodObject(pvc.Name)
+					pod = f.GetPodObject(podName1, pvc.Name)
 					err = f.CreatePod(pod)
 					Expect(err).NotTo(HaveOccurred())
 				})
 
 				AfterEach(func() {
 					By("Deleting the Pod with PVC")
-					err = f.DeletePod(pod.ObjectMeta)
+					err = f.DeletePod(pod.Name)
 					Expect(err).NotTo(HaveOccurred())
 
 					By("Waiting for the Volume to be Detached")
-					time.Sleep(2*time.Minute)
+					waitForOperation()
 
 					By("Deleting the PVC")
 					err = f.DeletePersistentVolumeClaim(pvc.ObjectMeta)
 					Expect(err).NotTo(HaveOccurred())
 
 					By("Waiting for the Volume to be Deleted")
-					time.Sleep(1*time.Minute)
+					waitForOperation()
 				})
 
 				Context("1Gi Storage", func() {
@@ -90,6 +112,71 @@ var _ = Describe("CSIDriver", func() {
 					})
 					It("should write and read", func() {
 						writeFile(file)
+						readFile(file)
+					})
+				})
+			})
+		})
+
+		Context("Pre-Provisioned", func() {
+			Context("Linode Block Storage", func() {
+				JustBeforeEach(func() {
+					By("Creating Persistent Volume Claim")
+					pvc = f.GetPersistentVolumeClaim(size)
+					err = f.CreatePersistentVolumeClaim(pvc)
+					Expect(err).NotTo(HaveOccurred())
+
+					By("Creating Pod with PVC")
+					pod = f.GetPodObject(podName1, pvc.Name)
+					err = f.CreatePod(pod)
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				AfterEach(func() {
+					By("Deleting the Pod with PVC")
+					err = f.DeletePod(pod.Name)
+					Expect(err).NotTo(HaveOccurred())
+
+					By("Waiting for the Volume to be Detached")
+					waitForOperation()
+
+					By("Deleting the PVC")
+					err = f.DeletePersistentVolumeClaim(pvc.ObjectMeta)
+					Expect(err).NotTo(HaveOccurred())
+
+					By("Waiting for the Volume to be Deleted")
+					waitForOperation()
+				})
+
+				Context("10Gi Storage", func() {
+					BeforeEach(func() {
+						size = "10Gi"
+					})
+					It("should write and read", func() {
+						writeFile(file)
+						deleteAndCreatePod(podName2)
+						readFile(file)
+					})
+				})
+
+				Context("15Gi Storage", func() {
+					BeforeEach(func() {
+						size = "15Gi"
+					})
+					It("should write and read", func() {
+						writeFile(file)
+						deleteAndCreatePod(podName2)
+						readFile(file)
+					})
+				})
+
+				Context("20Gi Storage", func() {
+					BeforeEach(func() {
+						size = "20Gi"
+					})
+					It("should write and read", func() {
+						writeFile(file)
+						deleteAndCreatePod(podName2)
 						readFile(file)
 					})
 				})
--- e2e/test/go.mod
+++ e2e/test/go.mod
@@ -5,6 +5,8 @@ go 1.12
 require (
 	github.com/appscode/go v0.0.0-20190112082056-52eaa8008e2e
 	github.com/docker/spdystream v0.0.0-20181023171402-6480d4af844c // indirect
+	github.com/elazarl/goproxy v0.0.0-20190410145444-c548f45dcf1d // indirect
+	github.com/elazarl/goproxy/ext v0.0.0-20190410145444-c548f45dcf1d // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/gogo/protobuf v1.2.1 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
@@ -17,13 +19,12 @@ require (
 	github.com/onsi/gomega v1.5.0
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pkg/errors v0.8.0
-	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a // indirect
+	golang.org/x/net v0.0.0-20190108225652-1e06a53dbb7e // indirect
+	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	k8s.io/api v0.0.0-20180904230853-4e7be11eab3f
 	k8s.io/apimachinery v0.0.0-20180621070125-103fd098999d
 	k8s.io/client-go v8.0.0+incompatible
-	k8s.io/klog v0.2.0 // indirect
-	k8s.io/utils v0.0.0-20181115163542-0d26856f57b3
 	kmodules.xyz/client-go v0.0.0-20190330080556-5dcc4cecf4c9
 )
