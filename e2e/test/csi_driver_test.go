4fed1d77cd1be2701fcc8d98b9576cd77eb4c11b
Luis Ramirez <luis.ramirez.rivera92@gmail.com>
2022-01-26 10:57:50 -0500

    Add e2e test for StatefulSet w/ PVC
    The test ensures that a StatefulSet with a PVC preserves
    the data outside of the Pod in a Linode BS Volume.
    
    It performs this test using a non-root container.

--- e2e/test/csi_driver_test.go
+++ e2e/test/csi_driver_test.go
@@ -2,178 +2,265 @@ package test
 
 import (
 	"e2e_test/test/framework"
+	"fmt"
 	"strconv"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
 	core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var _ = Describe("CSIDriver", func() {
-	var (
-		err          error
-		pod          *core.Pod
-		pvc          *core.PersistentVolumeClaim
-		f            *framework.Invocation
-		size         string
-		file         = "/data/heredoc"
-		storageClass = "linode-block-storage"
-	)
-	BeforeEach(func() {
-		f = root.Invoke()
-	})
+var _ = Describe("Linode CSI Driver", func() {
+	Describe("A StatefulSet with a PVC", func() {
+		Context("Using VolumeClaimTemplates and a non-root container", func() {
+			var (
+				f            *framework.Invocation
+				storageClass = "linode-block-storage"
+				sts          *appsv1.StatefulSet
+				pod          *core.Pod
+				pvc          *core.PersistentVolumeClaim
+				file         = "/data/file.txt"
+			)
 
-	var writeFile = func(filename string) {
-		By("Writing a File into the Pod")
-		err = f.WriteFileIntoPod(filename, pod)
-		Expect(err).NotTo(HaveOccurred())
-	}
+			BeforeEach(func() {
+				f = root.Invoke()
+				By("Getting the StatefulSet manifest w/ non-root container")
+				sts = f.GetStatefulSetObject("redis-test", f.Namespace(), storageClass)
 
-	var readFile = func(filename string) {
-		By("Checking the Created File into the Pod")
-		err = f.CheckFileIntoPod(filename, pod)
-		Expect(err).NotTo(HaveOccurred())
-	}
+				By("Creating the StatefulSet in the cluster")
+				Eventually(func() error {
+					return f.CreateStatefulSet(sts)
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
 
-	var expandVolume = func(size string) {
-		By("Expanding Size of the Persistent Volume")
-		currentPVC, err := f.GetPersistentVolumeClaim(pvc.ObjectMeta)
-		Expect(err).NotTo(HaveOccurred())
+				By("Waiting until the StatefulSet Pod is healthy")
+				Eventually(func() error {
+					var err error
+					pod, err = f.GetPod("redis-test-0", f.Namespace())
+					if err != nil {
+						return err
+					}
+					return f.WaitForReady(pod.ObjectMeta)
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
 
-		currentPVC.Spec.Resources.Requests = core.ResourceList{
-			core.ResourceName(core.ResourceStorage): resource.MustParse(size),
-		}
-		err = f.UpdatePersistentVolumeClaim(currentPVC)
-		Expect(err).NotTo(HaveOccurred())
+				By("Checking that there is a PVC created for the StatefulSet")
+				Eventually(func() error {
+					var err error
+					pvc, err = f.GetPersistentVolumeClaim(metav1.ObjectMeta{Name: "data-redis-test-0", Namespace: f.Namespace()})
+					if err != nil {
+						return err
+					}
+					return nil
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
+			})
 
-		By("Checking if Volume Expansion Occurred")
-		Eventually(func() string {
-			s, _ := f.GetVolumeSize(currentPVC)
-			return strconv.Itoa(s) + "Gi"
-		}, f.Timeout, f.RetryInterval).Should(Equal(size))
-	}
+			AfterEach(func() {
+				By("Deleting the StatefulSet")
+				Eventually(func() error {
+					return f.DeleteStatefulSet(sts.ObjectMeta)
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
 
-	Describe("Test", func() {
-		Context("Simple", func() {
-			Context("Block Storage", func() {
-				JustBeforeEach(func() {
-					By("Creating Persistent Volume Claim")
-					pvc = f.GetPersistentVolumeClaimObject(size, storageClass)
-					err = f.CreatePersistentVolumeClaim(pvc)
-					Expect(err).NotTo(HaveOccurred())
+				By("Getting the Volume information")
+				var (
+					currentPVC *core.PersistentVolumeClaim
+					volumeID   int
+				)
+				Eventually(func() error {
+					var err error
+					currentPVC, err = f.GetPersistentVolumeClaim(pvc.ObjectMeta)
+					if err != nil {
+						return err
+					}
+					volumeID, err = f.GetVolumeID(currentPVC)
+					if err != nil {
+						return err
+					}
+					return nil
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
 
-					By("Creating Pod with PVC")
-					pod = f.GetPodObject(pvc.Name)
-					err = f.CreatePod(pod)
-					Expect(err).NotTo(HaveOccurred())
-				})
+				By("Waiting for the Volume to be Detached")
+				Eventually(func() bool {
+					isAttached, err := f.IsVolumeDetached(volumeID)
+					if err != nil {
+						return false
+					}
+					return isAttached
+				}, f.Timeout, f.RetryInterval).Should(BeTrue())
 
-				AfterEach(func() {
-					By("Deleting the Pod with PVC")
-					err = f.DeletePod(pod.ObjectMeta)
-					Expect(err).NotTo(HaveOccurred())
+				By("Deleting the PVC")
+				Eventually(func() error {
+					return f.DeletePersistentVolumeClaim(pvc.ObjectMeta)
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
 
-					By("Getting the Volume information")
-					currentPVC, err := f.GetPersistentVolumeClaim(pvc.ObjectMeta)
-					Expect(err).NotTo(HaveOccurred())
-					volumeID, err := f.GetVolumeID(currentPVC)
-					Expect(err).NotTo(HaveOccurred())
+				By("Waiting for the Volume to be Deleted")
+				Eventually(func() bool {
+					isDeleted, err := f.IsVolumeDeleted(volumeID)
+					if err != nil {
+						return false
+					}
+					return isDeleted
+				}, f.Timeout, f.RetryInterval).Should(BeTrue())
+			})
 
-					By("Waiting for the Volume to be Detached")
-					Eventually(func() bool {
-						isAttached, err := f.IsVolumeDetached(volumeID)
-						if err != nil {
-							return false
-						}
-						return isAttached
-					}, f.Timeout, f.RetryInterval).Should(BeTrue())
+			It("Ensures no data is lost between Pod deletions", func() {
+				var err error
+				By("Saving a file in the mounted directory within the container")
+				err = f.WriteFileIntoPod(file, pod)
+				Expect(err).NotTo(HaveOccurred())
 
-					By("Deleting the PVC")
-					err = f.DeletePersistentVolumeClaim(pvc.ObjectMeta)
-					Expect(err).NotTo(HaveOccurred())
+				By("Deleting the StatefulSet Pod")
+				Eventually(func() error {
+					return f.DeletePod(pod.ObjectMeta)
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
 
-					By("Waiting for the Volume to be Deleted")
-					Eventually(func() bool {
-						isDeleted, err := f.IsVolumeDeleted(volumeID)
-						if err != nil {
-							return false
-						}
-						return isDeleted
-					}, f.Timeout, f.RetryInterval).Should(BeTrue())
-				})
+				By("Waiting until the StatefulSet Pod is recreated")
+				Eventually(func() error {
+					name := "redis-test-0"
+					p, err := f.GetPod(name, f.Namespace())
+					if err != nil {
+						return err
+					}
+					if p.ObjectMeta.UID == pod.ObjectMeta.UID {
+						return fmt.Errorf("pod %s/%s not deleted", f.Namespace(), name)
+					}
+					pod = p
+					return nil
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
+				Eventually(func() error {
+					return f.WaitForReady(pod.ObjectMeta)
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
 
-				Context("1Gi Storage", func() {
-					BeforeEach(func() {
-						size = "1Gi"
-					})
-					It("should write and read", func() {
-						writeFile(file)
-						readFile(file)
-					})
-				})
+				By("Checking that the file is still present inside the container")
+				err = f.CheckIfFileIsInPod(file, pod)
+				Expect(err).NotTo(HaveOccurred())
 			})
 		})
 	})
 
-	Describe("Test", func() {
-		Context("Block Storage", func() {
-			Context("Volume Expansion", func() {
-				JustBeforeEach(func() {
-					By("Creating Persistent Volume Claim")
-					pvc = f.GetPersistentVolumeClaimObject(size, storageClass)
-					err = f.CreatePersistentVolumeClaim(pvc)
-					Expect(err).NotTo(HaveOccurred())
+	Describe("A Pod with a PVC", func() {
+		var (
+			f            *framework.Invocation
+			pod          *core.Pod
+			pvc          *core.PersistentVolumeClaim
+			size         string
+			file         = "/data/heredoc"
+			storageClass = "linode-block-storage"
+		)
 
-					By("Creating Pod with PVC")
-					pod = f.GetPodObject(pvc.Name)
-					err = f.CreatePod(pod)
-					Expect(err).NotTo(HaveOccurred())
+		var writeFile = func(filename string) {
+			By("Writing a file into the Pod")
+			err := f.WriteFileIntoPod(filename, pod)
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		var readFile = func(filename string) {
+			By("Checking if the created file is in the Pod")
+			err := f.CheckIfFileIsInPod(filename, pod)
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		var expandVolume = func(size string) {
+			By("Expanding size of the Persistent Volume")
+			currentPVC, err := f.GetPersistentVolumeClaim(pvc.ObjectMeta)
+			Expect(err).NotTo(HaveOccurred())
+
+			currentPVC.Spec.Resources.Requests = core.ResourceList{
+				core.ResourceName(core.ResourceStorage): resource.MustParse(size),
+			}
+			err = f.UpdatePersistentVolumeClaim(currentPVC)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Checking if Volume expansion occurred")
+			Eventually(func() string {
+				s, _ := f.GetVolumeSize(currentPVC)
+				return strconv.Itoa(s) + "Gi"
+			}, f.Timeout, f.RetryInterval).Should(Equal(size))
+		}
+
+		Context("Using a Pod with a PVC mounted", func() {
+			JustBeforeEach(func() {
+				f = root.Invoke()
+				By("Creating the Persistent Volume Claim")
+				pvc = f.GetPersistentVolumeClaimObject("test-pvc", f.Namespace(), size, storageClass)
+				Eventually(func() error {
+					return f.CreatePersistentVolumeClaim(pvc)
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
+
+				By("Creating Pod with PVC")
+				pod = f.GetPodObject("busybox-test", f.Namespace(), pvc.Name)
+				Eventually(func() error {
+					return f.CreatePod(pod)
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
+			})
+
+			AfterEach(func() {
+				By("Deleting the Pod with PVC")
+				Eventually(func() error {
+					return f.DeletePod(pod.ObjectMeta)
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
+
+				By("Getting the Volume information")
+				var (
+					currentPVC *core.PersistentVolumeClaim
+					volumeID   int
+				)
+				Eventually(func() error {
+					var err error
+					currentPVC, err = f.GetPersistentVolumeClaim(pvc.ObjectMeta)
+					if err != nil {
+						return err
+					}
+					volumeID, err = f.GetVolumeID(currentPVC)
+					if err != nil {
+						return err
+					}
+					return nil
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
+
+				By("Waiting for the Volume to be Detached")
+				Eventually(func() bool {
+					isAttached, err := f.IsVolumeDetached(volumeID)
+					if err != nil {
+						return false
+					}
+					return isAttached
+				}, f.Timeout, f.RetryInterval).Should(BeTrue())
+
+				By("Deleting the PVC")
+				Eventually(func() error {
+					return f.DeletePersistentVolumeClaim(pvc.ObjectMeta)
+				}, f.Timeout, f.RetryInterval).Should(Succeed())
+
+				By("Waiting for the Volume to be Deleted")
+				Eventually(func() bool {
+					isDeleted, err := f.IsVolumeDeleted(volumeID)
+					if err != nil {
+						return false
+					}
+					return isDeleted
+				}, f.Timeout, f.RetryInterval).Should(BeTrue())
+			})
+
+			Context("1Gi Storage", func() {
+				BeforeEach(func() {
+					size = "1Gi"
 				})
-
-				AfterEach(func() {
-					By("Deleting the Pod with PVC")
-					err = f.DeletePod(pod.ObjectMeta)
-					Expect(err).NotTo(HaveOccurred())
-
-					By("Getting the Volume information")
-					currentPVC, err := f.GetPersistentVolumeClaim(pvc.ObjectMeta)
-					Expect(err).NotTo(HaveOccurred())
-					volumeID, err := f.GetVolumeID(currentPVC)
-					Expect(err).NotTo(HaveOccurred())
-
-					By("Waiting for the Volume to be Detached")
-					Eventually(func() bool {
-						isDetached, err := f.IsVolumeDetached(volumeID)
-						if err != nil {
-							return false
-						}
-						return isDetached
-					}, f.Timeout, f.RetryInterval).Should(BeTrue())
-
-					By("Deleting the PVC")
-					err = f.DeletePersistentVolumeClaim(pvc.ObjectMeta)
-					Expect(err).NotTo(HaveOccurred())
-
-					By("Waiting for the Volume to be Deleted")
-					Eventually(func() bool {
-						isDeleted, err := f.IsVolumeDeleted(volumeID)
-						if err != nil {
-							return false
-						}
-						return isDeleted
-					}, f.Timeout, f.RetryInterval).Should(BeTrue())
+				It("should write and read", func() {
+					writeFile(file)
+					readFile(file)
 				})
+			})
 
-				Context("Expanding Storage from 10Gi to 15Gi", func() {
-					BeforeEach(func() {
-						size = "10Gi"
-					})
-					It("should write and read", func() {
-						writeFile(file)
-						expandVolume("15Gi")
-						readFile(file)
-					})
+			Context("Expanding Storage from 10Gi to 15Gi", func() {
+				BeforeEach(func() {
+					size = "10Gi"
+				})
+				It("should write and read", func() {
+					writeFile(file)
+					expandVolume("15Gi")
+					readFile(file)
 				})
 			})
 		})
--- e2e/test/framework/pod.go
+++ e2e/test/framework/pod.go
@@ -2,6 +2,7 @@ package framework
 
 import (
 	"fmt"
+
 	"github.com/appscode/go/wait"
 	"github.com/pkg/errors"
 	core "k8s.io/api/core/v1"
@@ -9,16 +10,16 @@ import (
 	"kmodules.xyz/client-go/tools/exec"
 )
 
-func (f *Invocation) GetPodObject(pvc string) *core.Pod {
+func (f *Invocation) GetPodObject(name, namespace, pvc string) *core.Pod {
 	return &core.Pod{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      f.app,
-			Namespace: f.namespace,
+			Name:      name,
+			Namespace: namespace,
 		},
 		Spec: core.PodSpec{
 			Containers: []core.Container{
 				{
-					Name:  f.app,
+					Name:  name,
 					Image: "busybox",
 					VolumeMounts: []core.VolumeMount{
 						{
@@ -44,7 +45,7 @@ func (f *Invocation) GetPodObject(pvc string) *core.Pod {
 }
 
 func (f *Invocation) CreatePod(pod *core.Pod) error {
-	pod, err := f.kubeClient.CoreV1().Pods(f.namespace).Create(pod)
+	pod, err := f.kubeClient.CoreV1().Pods(pod.ObjectMeta.Namespace).Create(pod)
 	if err != nil {
 		return err
 	}
@@ -53,16 +54,16 @@ func (f *Invocation) CreatePod(pod *core.Pod) error {
 }
 
 func (f *Invocation) DeletePod(meta metav1.ObjectMeta) error {
-	return f.kubeClient.CoreV1().Pods(f.namespace).Delete(meta.Name, deleteInForeground())
+	return f.kubeClient.CoreV1().Pods(meta.Namespace).Delete(meta.Name, deleteInForeground())
 }
 
-func (f *Invocation) GetPod(name, ns string) (*core.Pod, error) {
-	return f.kubeClient.CoreV1().Pods(ns).Get(name, metav1.GetOptions{})
+func (f *Invocation) GetPod(name, namespace string) (*core.Pod, error) {
+	return f.kubeClient.CoreV1().Pods(namespace).Get(name, metav1.GetOptions{})
 }
 
 func (f *Invocation) WaitForReady(meta metav1.ObjectMeta) error {
 	return wait.PollImmediate(f.RetryInterval, f.Timeout, func() (bool, error) {
-		pod, err := f.kubeClient.CoreV1().Pods(f.namespace).Get(meta.Name, metav1.GetOptions{})
+		pod, err := f.kubeClient.CoreV1().Pods(meta.Namespace).Get(meta.Name, metav1.GetOptions{})
 		if pod == nil || err != nil {
 			return false, nil
 		}
@@ -81,7 +82,7 @@ func (f *Invocation) WriteFileIntoPod(filename string, pod *core.Pod) error {
 	return err
 }
 
-func (f *Invocation) CheckFileIntoPod(filename string, pod *core.Pod) error {
+func (f *Invocation) CheckIfFileIsInPod(filename string, pod *core.Pod) error {
 	out, err := exec.ExecIntoPod(f.restConfig, pod, exec.Command([]string{
 		"ls", filename,
 	}...))
