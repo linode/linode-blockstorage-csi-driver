129036182f0d6853e88a05842e5baa39f8017bba
Nick Saika <nsaika@akamai.com>
2024-06-18 18:45:37 +0000

    internal/driver: Simplify set up of driver capabilities
    The driver's capabilities were currently split up across several
    functions, across several files. This change consolidates all of the
    capabilities handling into one function per capability type (controller,
    node, volume access modes).

--- internal/driver/capabilities.go
+++ internal/driver/capabilities.go
@@ -2,32 +2,67 @@ package driver
 
 import "github.com/container-storage-interface/spec/lib/go/csi"
 
-func controllerCapabilities() []csi.ControllerServiceCapability_RPC_Type {
-	return []csi.ControllerServiceCapability_RPC_Type{
+// ControllerServiceCapabilities returns the list of capabilities supported by
+// this driver's controller service.
+func ControllerServiceCapabilities() []*csi.ControllerServiceCapability {
+	capabilities := []csi.ControllerServiceCapability_RPC_Type{
 		csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME,
 		csi.ControllerServiceCapability_RPC_PUBLISH_UNPUBLISH_VOLUME,
-		// csi.ControllerServiceCapability_RPC_CREATE_DELETE_SNAPSHOT,
-		// csi.ControllerServiceCapability_RPC_LIST_SNAPSHOTS,
 		csi.ControllerServiceCapability_RPC_PUBLISH_READONLY,
 		csi.ControllerServiceCapability_RPC_EXPAND_VOLUME,
 		csi.ControllerServiceCapability_RPC_CLONE_VOLUME,
 		csi.ControllerServiceCapability_RPC_LIST_VOLUMES,
 		csi.ControllerServiceCapability_RPC_VOLUME_CONDITION,
 	}
+
+	cc := make([]*csi.ControllerServiceCapability, 0, len(capabilities))
+	for _, c := range capabilities {
+		cc = append(cc, &csi.ControllerServiceCapability{
+			Type: &csi.ControllerServiceCapability_Rpc{
+				Rpc: &csi.ControllerServiceCapability_RPC{
+					Type: c,
+				},
+			},
+		})
+	}
+	return cc
 }
 
-func nodeCapabilities() []csi.NodeServiceCapability_RPC_Type {
-	return []csi.NodeServiceCapability_RPC_Type{
+// NodeServiceCapabilities returns the list of capabilities supported by this
+// driver's node service.
+func NodeServiceCapabilities() []*csi.NodeServiceCapability {
+	capabilities := []csi.NodeServiceCapability_RPC_Type{
 		csi.NodeServiceCapability_RPC_STAGE_UNSTAGE_VOLUME,
 		csi.NodeServiceCapability_RPC_EXPAND_VOLUME,
 		csi.NodeServiceCapability_RPC_GET_VOLUME_STATS,
 		csi.NodeServiceCapability_RPC_VOLUME_CONDITION,
 	}
+
+	cc := make([]*csi.NodeServiceCapability, 0, len(capabilities))
+	for _, c := range capabilities {
+		cc = append(cc, &csi.NodeServiceCapability{
+			Type: &csi.NodeServiceCapability_Rpc{
+				Rpc: &csi.NodeServiceCapability_RPC{
+					Type: c,
+				},
+			},
+		})
+	}
+	return cc
 }
 
-func volumeCapabilitiesAccessMode() []csi.VolumeCapability_AccessMode_Mode {
-	return []csi.VolumeCapability_AccessMode_Mode{
+// VolumeCapabilityAccessModes returns the allowed access modes for a volume
+// created by the driver.
+func VolumeCapabilityAccessModes() []*csi.VolumeCapability_AccessMode {
+	modes := []csi.VolumeCapability_AccessMode_Mode{
 		csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER,
-		// csi.VolumeCapability_AccessMode_MULTI_NODE_READER_ONLY,
 	}
+
+	mm := make([]*csi.VolumeCapability_AccessMode, 0, len(modes))
+	for _, m := range modes {
+		mm = append(mm, &csi.VolumeCapability_AccessMode{
+			Mode: m,
+		})
+	}
+	return mm
 }
