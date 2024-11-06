e393a03b12553660248653cc780b3d8041e9cc85
Khaja Omer <komer@akamai.com>
2024-11-06 09:31:13 -0600

    bug fix: volumeContext[VolumeTopologyRegion] was not a reliable source to get volumes region. Switching to just use volume obj returned by API to validate is a better and more robust approach

--- internal/driver/controllerserver_helper.go
+++ internal/driver/controllerserver_helper.go
@@ -584,8 +584,8 @@ func (cs *ControllerServer) getAndValidateVolume(ctx context.Context, volumeID i
 	}
 
 	// check if the volume and instance are in the same region
-	if instance.Region != volContext[VolumeTopologyRegion] {
-		return "", errRegionMismatch(volContext[VolumeTopologyRegion], instance.Region)
+	if instance.Region != volume.Region {
+		return "", errRegionMismatch(volume.Region, instance.Region)
 	}
 
 	log.V(4).Info("Volume validated and is not attached to instance", "volume_id", volume.ID, "node_id", instance.ID)
